2722064528644d8d32ca509bf4b7d276dba522eb
Gabriel Goulart <gabriel.gsouzza6@gmail.com>
2025-05-13 19:01:31 -0300

    Pet APP

--- app/src/main/java/com/example/petapp/MostrarPetActivity.java
+++ app/src/main/java/com/example/petapp/MostrarPetActivity.java
@@ -1,6 +1,7 @@
 package com.example.petapp;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -8,14 +9,14 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.widget.NestedScrollView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.app.Activity.ScreenCaptureCallback;
 
 public class MostrarPetActivity extends AppCompatActivity {
 
     private TextView voltar;
-    private NestedScrollView rolar;
-    private ImageView frente;
-    private ImageView verso;
+    private ViewPager2 viewPager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,6 +24,8 @@ public class MostrarPetActivity extends AppCompatActivity {
         setContentView(R.layout.mostrar_pet);
 
         voltar = findViewById(R.id.voltar);
+        viewPager = findViewById(R.id.rolar);
+
         voltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,10 +33,6 @@ public class MostrarPetActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-        rolar = findViewById(R.id.rolar);
-        frente = findViewById(R.id.frente);
-        verso = findViewById(R.id.verso);
 
     }
 }
