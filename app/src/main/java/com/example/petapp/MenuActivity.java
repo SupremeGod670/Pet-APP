85a384548b44afed925c8a9393a789390e6d8df8
Gabriel Goulart <gabriel.gsouzza6@gmail.com>
2025-05-26 16:36:17 -0300

    Intent da lista pet para o mostrar funionando certinho.

--- app/src/main/java/com/example/petapp/MenuActivity.java
+++ app/src/main/java/com/example/petapp/MenuActivity.java
@@ -1,107 +1,121 @@
-    package com.example.petapp;
+package com.example.petapp;
 
-    import android.app.Activity;
-    import android.content.Intent;
-    import android.os.Bundle;
-    import android.view.MenuItem;
-    import android.view.View;
-    import android.widget.Button;
-    import android.widget.ImageButton;
-    import android.widget.ListView;
-    import android.widget.Spinner;
-    import android.widget.TextView;
-    import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-    import androidx.annotation.NonNull;
-    import androidx.annotation.Nullable;
-    import androidx.appcompat.app.AppCompatActivity;
-    import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-    import com.example.petapp.adapter.CriarPetsModel;
-    import com.example.petapp.adapter.PetAdapter;
-    import com.example.petapp.database.databasePet.dao.RegistroPetDAO;
-    import com.example.petapp.database.databasePet.model.RegistroPetModel;
-    import com.example.petapp.database.databaseUser.dao.RegistroUserDAO;
-    import com.example.petapp.database.databaseUser.model.RegistroUserModel;
-    import com.google.android.material.navigation.NavigationView;
+import com.example.petapp.adapter.CriarPetsModel;
+import com.example.petapp.adapter.PetAdapter;
+import com.example.petapp.database.databasePet.dao.RegistroPetDAO;
+import com.google.android.material.navigation.NavigationView;
 
-    import com.example.petapp.LoginActivity;
-    import com.example.petapp.CriarPetsActivity;
+import java.util.ArrayList;
 
-    import java.util.ArrayList;
+public class MenuActivity extends AppCompatActivity {
 
-    public class MenuActivity extends AppCompatActivity {
+    private ListView listpet1, listpet2;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Button sair;
+    ImageButton open;
+    TextView criar, nomePet;
+    Spinner editraca;
+    LinearLayout pet;
 
-        private ListView listpet1, listpet2;
-        DrawerLayout drawerLayout;
-        NavigationView navigationView;
-        Button sair;
-        ImageButton open;
-        TextView criar, nomePet;
-        Spinner editraca;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
 
-        @Override
-        protected void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_menu);
+        criar = findViewById(R.id.criar);
 
-            criar = findViewById(R.id.criar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        sair = findViewById(R.id.sair);
+        open = findViewById(R.id.open);
 
-            drawerLayout = findViewById(R.id.drawer_layout);
-            navigationView = findViewById(R.id.nav_view);
-            sair = findViewById(R.id.sair);
-            open = findViewById(R.id.open);
+        listpet1 = findViewById(R.id.listpet1);
+        listpet2 = findViewById(R.id.listpet2);
 
-            listpet1 = findViewById(R.id.listpet1);
-            listpet2 = findViewById(R.id.listpet2);
+        pet = findViewById(R.id.pet);
 
-            open.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    drawerLayout.open();
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.open();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_logout) {
+                    Toast.makeText(MenuActivity.this, "Configurações Clicado", Toast.LENGTH_SHORT).show();
+
                 }
-            });
 
-            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.close();
 
-                    int itemId = item.getItemId();
+                return false;
+            }
+        });
 
-                    if(itemId == R.id.nav_logout){
-                        Toast.makeText(MenuActivity.this, "Configurações Clicado", Toast.LENGTH_SHORT).show();
+        sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
-                    }
+        RegistroPetDAO dao = new RegistroPetDAO(this);
 
-                    drawerLayout.close();
+        ArrayList<CriarPetsModel> getPets = new ArrayList<>();
+        getPets.add(new CriarPetsModel("", "" + dao.selectNome(), "" + dao.selectRaca()));
 
-                    return false;
-                }
-            });
+        PetAdapter adapter = new PetAdapter(this, getPets);
+        listpet1.setAdapter(adapter);
 
-            sair.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MenuActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                }
-            });
+        listpet1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MenuActivity.this, MostrarPetActivity.class);
+                startActivity(intent);
+            }
+        });
 
-            RegistroPetDAO dao = new RegistroPetDAO(this);
+        listpet2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MenuActivity.this, MostrarPetActivity.class);
+                startActivity(intent);
+            }
+        });
 
-            ArrayList<CriarPetsModel> getPets = new ArrayList<>();
-            getPets.add(new CriarPetsModel("", "" + dao.selectNome(), "" + dao.selectRaca()));
+        criar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, CriarPetsActivity.class);
+                startActivity(intent);
+            }
+        });
 
-            PetAdapter adapter = new PetAdapter(this, getPets);
-            listpet1.setAdapter(adapter);
-
-            criar.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(MenuActivity.this, CriarPetsActivity.class);
-                    startActivity(intent);
-                }
-            });
-
-        }
     }
+}
