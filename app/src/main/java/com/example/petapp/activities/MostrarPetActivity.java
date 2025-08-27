0ea2f6b57cdb5e276271b96155f0d96af603651c
Gabriel-GDS-dev <gabriel.gsouzza6@gmail.com>
2025-08-27 15:39:37 -0300

    Botão deletar funcionando

--- app/src/main/java/com/example/petapp/activities/MostrarPetActivity.java
+++ app/src/main/java/com/example/petapp/activities/MostrarPetActivity.java
@@ -3,7 +3,10 @@ package com.example.petapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,19 +15,21 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.example.petapp.fragments.Fragment1;
-import com.example.petapp.fragments.Fragment2;
 import com.example.petapp.R;
 import com.example.petapp.database.databasePet.dao.RegistroPetDAO;
 import com.example.petapp.database.databasePet.model.RegistroPetModel;
+import com.example.petapp.fragments.Fragment1;
+import com.example.petapp.fragments.Fragment2;
 
 public class MostrarPetActivity extends AppCompatActivity {
 
     private TextView voltar;
     private static final int NUM_PAGES = 2;
     private ViewPager2 viewPager;
+    private Button editarpet, deletarpet;
     private FragmentStateAdapter pagerAdapter;
     private RegistroPetModel petAtual;
+    RegistroPetDAO dao;
 
     public static final String EXTRA_PET_ID = "EXTRA_PET_ID";
 
@@ -35,6 +40,8 @@ public class MostrarPetActivity extends AppCompatActivity {
 
         voltar = findViewById(R.id.voltar);
         viewPager = findViewById(R.id.rolar);
+        editarpet = findViewById(R.id.editarpet);
+        deletarpet = findViewById(R.id.deletarpet);
 
         // Buscar dados do pet
         Long petId = getIntent().getLongExtra("PET_ID", -1L);
@@ -111,6 +118,23 @@ public class MostrarPetActivity extends AppCompatActivity {
             }
         };
         viewPager.setAdapter(pagerAdapter);
+
+        editarpet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        deletarpet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    dao = new RegistroPetDAO(MostrarPetActivity.this);
+                    dao.delete(petId);
+                    finish();
+                    Toast.makeText(MostrarPetActivity.this, "Pet deletado com sucesso!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public RegistroPetModel getPetAtual() {
